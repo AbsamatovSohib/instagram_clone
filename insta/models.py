48652fe8ac1250f55aea5b1055791b6e76c34caf
AbsamatovSohib <sakhibdevelopers2000@gmail.com>
2024-03-05 19:06:38 +0500

    fix: models

--- insta/models.py
+++ insta/models.py
@@ -11,16 +11,23 @@ class Profile(BaseModel):
     gender = models.CharField(choices=GenderTypes.choices, default=GenderTypes.MALE, max_length=30)
     bio = models.CharField(max_length=150)
 
+    is_public = models.BooleanField(default=True)
+
     # def __str__(self):
     #     return self.user.id
+
+
+class Images(BaseModel):
+    image = models.ImageField(upload_to="post/", null=True, blank=True, validators=[
+        FileExtensionValidator(allowed_extensions=["IMG, SVG, PNG, JPEG, GIF"])
+    ])
 
 
 class Post(BaseModel):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="owner")
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to="post/", null=True, blank=True, validators=[
-        FileExtensionValidator(allowed_extensions=["IMG, SVG, PNG, JPEG, GIF"])
-                              ])
+
+    image = models.ManyToManyField(Images, related_name="images_for_post")
     video = models.FileField(upload_to='videos_uploaded', null=True, blank=True,
                              validators=[
                                  FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv'])])
@@ -50,3 +57,23 @@ class LikedPost(BaseModel):
 class SavedPost(BaseModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="saved_posts")
     saved_by = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="saved_by_owner")
+
+
+class Followers(BaseModel):
+    followed_by = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="followed_by")
+    followed_to = models.ManyToManyField(Profile, related_name="followed_to")
+
+
+class Following(BaseModel):
+    followed_by = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="following_by")
+    followed_to = models.ManyToManyField(Profile, related_name="following_to")
+
+
+class Story(BaseModel):
+    owner = models.ForeignKey(Profile, related_name="story_owner")
+    post_as_story = models.ForeignKey(Post, related_name="story_post", null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    mention = models.ManyToManyField(Profile, related_name="mentioned")
+
+    description = models.CharField(max_length=256)
